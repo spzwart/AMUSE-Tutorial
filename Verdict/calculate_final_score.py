d978058586ad37058518b9ad893dac616240ad79
spz <spz@strw.leidenuniv.nl>
2025-12-23 12:39:29 +0100

    make input matric a bit more interesting

--- Verdict/calculate_final_score.py
+++ Verdict/calculate_final_score.py
@@ -7,6 +7,9 @@ def new_option_parser():
     result.add_option("-f", dest="input_filename", 
                       default = "../Scores/Group_1_scores.csv",
                       help="input filename [%default]")
+    result.add_option("--verbose", dest="verbose", 
+                      default = "0", type = "int", 
+                      help="verbosity [%default]")
     return result
     
 if __name__ in ('__main__', '__plot__'):
@@ -31,8 +34,14 @@ if __name__ in ('__main__', '__plot__'):
                 gitusage.append(float(row["value"]))
             elif row["category"]=="report":
                 report.append(float(row["value"]))
-            print(row)
-    print(f"{np.mean(presentation)} +/- {np.std(presentation)}")
-    print(f"{np.mean(gitusage)} +/- {np.std(gitusage)}")
-    print(f"{np.mean(report)} +/- {np.std(report)}")
+            if o.verbose:
+                print(row)
+    group = o.input_filename.split("/")[-1]
+    print(f"\nVerdict for the course in Simulation and Modeling in Astrophyiscs for group {group}")
+    print(f"Presentation: {np.mean(presentation)} +/- {np.std(presentation)}")
+    print(f"Git usage: {np.mean(gitusage)} +/- {np.std(gitusage)}")
+    print(f"Report: {np.mean(report)} +/- {np.std(report)}")
 
+    verdict_score = (np.mean(presentation) + np.mean(gitusage) + np.mean(report))/3.0
+    verdict_disp  = np.sqrt(np.std(presentation)**2 + np.std(gitusage)**2 + np.std(report)**2)/3.0
+    print(f"Final score = {verdict_score:0.1f} +/- {verdict_disp:0.1f}")
